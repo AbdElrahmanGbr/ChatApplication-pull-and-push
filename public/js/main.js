2fe4b7b491e87971aa40128604a3f4ccacf0e03b
AbdElrahmanGbr <abdelrahman.gbr.os@gmail.com>
2022-05-29 17:23:26 +0200

    emit and printing output messages

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,26 @@
+const chatFrom = document.getElementById('chat-form');
 const socket = io();
+// message from server
+socket.on('message', (message) => {
+    console.log(message);
+    outpurMessage(message);
+});
+
+//Message submit
+chatFrom.addEventListener('submit', (e) => {
+    e.preventDefault(); //prevent page refresh
+    //get message text
+    const msg = e.target.elements.msg.value;
+    //Emitting a message to the server
+    socket.emit('chatMessage', msg);
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
+});
+
+//output message to dom
+function outpurMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta"> Hlbessa <span>${message}</span></p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+}
--- server.js
+++ server.js
@@ -14,7 +14,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //run when a client connects
 io.on('connection', socket => {
-    console.log('New WebSocket connection');
+
+    // welcome current user
+    socket.emit('message', 'Welcome to the chat app');
+
+    //Broadcast when a user connects for all users except the one who connected (broadcast)
+    socket.broadcast.emit('message', 'A new user has joined the chat');
+
+    //Runs when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    });
+    //listen for chatMessage
+    socket.on('chatMessage', msg => {
+        io.emit('message', msg);
+    });
 });
 
 const PORT = 3000 || process.env.PORT;
